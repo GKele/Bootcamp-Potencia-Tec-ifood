ce8d57e0882ea1afb4c979c3b800012686be5f94
Geize Kele <ge.kele@hotmail.com>
2022-10-25 14:33:56 -0300

    Notas

--- atv03-array-e-repeticao/loops/src/br/com/dio/exercicios/loops/Ex02_Notas.java
+++ atv03-array-e-repeticao/loops/src/br/com/dio/exercicios/loops/Ex02_Notas.java
@@ -1,1 +1,27 @@
+package br.com.dio.exercicios.loops;
 
+import java.util.Scanner;
+
+/*
+Faça um programa que peça uma nota entre 0 e 10.
+Mostre uma mensagem caso o valor seja inválido (10 < nota < 0) e
+continue pedido até que o usuário informe um valor válido.
+*/
+
+public class Ex02_Notas {
+    public static void main (String[] args){
+        Scanner scan = new Scanner(System.in);
+
+        int nota;
+
+        System.out.println("Nota: ");
+        nota = scan.nextInt();
+
+        while(nota < 0 || nota > 10){
+            System.out.println("Nota INVÁLIDA, digite novamente!");
+            nota = scan.nextInt();
+        }
+
+        System.out.println("Nota VÁLIDA, programa finalizado!");
+    }
+}
